03c613807ec529934bec3f8fd0d5227610a4dcc5
ssIIIn <gomee312@naver.com>
2024-04-17 01:00:50 +0900

    지역메모리, 함수스코프, 재귀함수 파악하는 코드 추가

--- 15663.cpp
+++ 15663.cpp
@@ -14,36 +14,39 @@ vector<bool> isVisited; // 해당 숫자를 포함했는지(방문했는지) 파
 
 void DFS(int start, int count)
 {
+    // cout << 1 << "\n";
     // 원하는 숫자만큼 수열을 채웠으면 출력
     if(count == M)
     {
         for (int i = 0; i < M; i++)
             cout << number[i] << " ";
         cout << "\n";
+        return;
     }
-
     // 원하는 숫자만큼 수열을 채울때까지 수열 채우기
     else
     {
+        // cout << 2 << "\n";
         int finalNum = 0;
 
         for (int i = 1; i < N+1; i++)
         {
             // 해당 수열에 숫자를 포함하지 않았으면,
             // 이미 출력된 이전 수열의 같은 자리 숫자가 
-            // cout << "finalNum : " << finalNum <<" count : " << count << " i : " << i << " number[count] : " << number[count] << " sortNum[i] : " << sortNum[i] << "\n";
-
+            cout << "finalNum : " << finalNum <<" count : " << count << " i : " << i << " number[count] : " << number[count] << " sortNum[i] : " << sortNum[i] << "\n";
+            // cout << 3 << "\n";
             if (isVisited[i] == false && sortNum[i] != finalNum)
             {
+                // cout << 4 << "\n";
                 isVisited[i] = true;
                 number[count] = sortNum[i];
                 finalNum = sortNum[i];
                 DFS(i+1, count+1);
+                // cout << 5 << "\n";
                 // DFS가 한번 끝나고 다시 돌아올 때(백트래킹)
                 // isVisited를 false로 만들어줘서 number에는 숫자가 있더라도 없는 것처럼 구현
                 // 초기화!! 딱 초기화 같은 느낌
                 isVisited[i] = false;
-
             }
         }
     }
